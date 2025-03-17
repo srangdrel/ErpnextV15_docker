85739b74ef61ddfd482b75c5fa1ca976dcc3fa1b
srangdrel <sherubrangdrel800@gmail.com>
2025-03-17 06:49:26 +0000

    test 2025-03-17

---
+++ erpnext/support/doctype/ground_booking/ground_booking.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class GroundBooking(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		from_date: DF.Date | None
+		to_dtae: DF.Date | None
+	# end: auto-generated types
+	pass
